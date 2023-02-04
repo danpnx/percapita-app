2b0a3122335a522e273f03aaad6774ac09bfa250
Amanda Ilg <amanda.ilg04@gmail.com>
2023-02-04 17:04:29 -0300

    Criacao da Tela Principal de Profile e Button Dark Mode

--- androidApp/src/main/java/br/com/percapita/android/screens/profile/ProfileScreen.kt
+++ androidApp/src/main/java/br/com/percapita/android/screens/profile/ProfileScreen.kt
@@ -1,71 +1,53 @@
-package br.com.percapita.android.screens.profile
+package br.com.percapita.android.screens.configuration
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.percapita.android.MyApplicationTheme
-import br.com.percapita.android.R
-
+import br.com.percapita.android.components.BottomBarPreview
+import br.com.percapita.android.components.BottomBarPreviewDark
+import br.com.percapita.android.components.ButtonDarkModePreview
+import br.com.percapita.android.components.ButtonDarkModePreviewDark
 
 @Composable
-fun ProfileScreen(isSystemDarkTheme: Boolean) {
-    MyApplicationTheme() {
-        Surface(modifier = Modifier.fillMaxSize()) {
+fun ConfigurationScreen(darkTheme: Boolean) {
+    MyApplicationTheme(darkTheme) {
+        Surface(modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background,
+            contentColor = MaterialTheme.colors.onBackground) {
+
             Column(verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 50.dp)) {
-                Image(painter = painterResource(id = R.drawable.percapita), contentDescription = "Logotipo",
-                    contentScale = ContentScale.Fit)
-            }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.padding(16.dp, vertical = 130.dp)) {
-                val userName = remember { mutableStateOf(TextFieldValue()) }
-                val userEmail = remember { mutableStateOf(TextFieldValue()) }
-                val confirmNewPassowrd = remember { mutableStateOf(TextFieldValue())}
-                val oldPassword = remember { mutableStateOf(TextFieldValue()) }
-                val newPassword = remember { mutableStateOf(TextFieldValue()) }
-                val passwordVisible = remember { mutableStateOf(false) }
-
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 30.dp)){
                 Text(
-                    text = "Editar Perfil",
+                    text = "Perfil",
+                    fontSize = 25.sp,
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.fillMaxWidth(0.9f))
+                    modifier = Modifier.fillMaxWidth(0.8f))
+            }
 
-                Spacer(modifier = Modifier.height(16.dp))
+            if (darkTheme) ButtonDarkModePreviewDark() else ButtonDarkModePreview()
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(16.dp, vertical = 100.dp)) {
 
                 Text(
                     text = "Nome Completo:",
                     textAlign = TextAlign.Left,
                     modifier = Modifier.fillMaxWidth(0.9f))
-                OutlinedTextField(modifier = Modifier.fillMaxWidth(0.9f),
-                    value = userName.value,
-                    onValueChange = { userName.value = it },
-                    label = { Text(text = "Nome Completo") },
-                )
+                Text(
+                    text = "Nome completo",
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier.fillMaxWidth(0.9f))
 
                 Spacer(modifier = Modifier.height(16.dp))
 
@@ -73,96 +55,53 @@ fun ProfileScreen(isSystemDarkTheme: Boolean) {
                     text = "E-mail:",
                     textAlign = TextAlign.Left,
                     modifier = Modifier.fillMaxWidth(0.9f))
-                TextField(modifier = Modifier.fillMaxWidth(0.9f),
-                    value = userEmail.value,
-                    onValueChange = { userEmail.value = it },
-                    label = { Text(text = "E-mail") },
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
                 Text(
-                    text = "Senha atual:",
+                    text = "email@email.com",
                     textAlign = TextAlign.Left,
                     modifier = Modifier.fillMaxWidth(0.9f))
-                OutlinedTextField(modifier = Modifier.fillMaxWidth(0.9f),
-                    value = oldPassword.value,
-                    onValueChange = { oldPassword.value = it },
-                    label = { Text(text = "Senha atual") },
-                    visualTransformation = if(passwordVisible.value.not()) PasswordVisualTransformation() else VisualTransformation.None,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                    trailingIcon = {
-                        val icone = if(passwordVisible.value.not()) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                        val description = if(passwordVisible.value.not()) "Invisivel" else "Visivel"
-                        IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
-                            Icon(imageVector = icone , contentDescription = description)
-                        }
-                    }
-                )
 
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text(
-                    text = "Nova senha:",
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.fillMaxWidth(0.9f))
-                OutlinedTextField(modifier = Modifier.fillMaxWidth(0.9f),
-                    value = newPassword.value,
-                    onValueChange = { newPassword.value = it },
-                    label = { Text(text = "Nova Senha") },
-                    visualTransformation = if(passwordVisible.value.not()) PasswordVisualTransformation() else VisualTransformation.None,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                    trailingIcon = {
-                        val icone = if(passwordVisible.value.not()) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                        val description = if(passwordVisible.value.not()) "Invisivel" else "Visivel"
-                        IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
-                            Icon(imageVector = icone , contentDescription = description)
-                        }
-                    }
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text(
-                    text = "Confirmação da senha:",
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.fillMaxWidth(0.9f))
-                OutlinedTextField(modifier = Modifier.fillMaxWidth(0.9f),
-                    value = confirmNewPassowrd.value,
-                    onValueChange = { confirmNewPassowrd.value = it },
-                    label = { Text(text = "Confirmação da senha") },
-                    visualTransformation = if(passwordVisible.value.not()) PasswordVisualTransformation() else VisualTransformation.None,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                    trailingIcon = {
-                        val icone = if(passwordVisible.value.not()) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                        val description = if(passwordVisible.value.not()) "Invisivel" else "Visivel"
-                        IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
-                            Icon(imageVector = icone , contentDescription = description)
-                        }
-                    }
-                )
 
                 Spacer(modifier = Modifier.height(32.dp))
 
                 Button(onClick = { /*TODO*/ },
                     modifier = Modifier.fillMaxWidth(0.8f),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF04C457))
-                ){
-                    Text(text = "Salvar alterações", fontSize = 16.sp)
+                    colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary))
+                {
+                    Text(text = "Editar perfil", fontSize = 16.sp)
                 }
             }
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Spacer(modifier = Modifier.weight(1f))
+
+                Button(onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(0.8f),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary)
+                ){
+                    Text(text = "Sair", fontSize = 16.sp)
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                if (darkTheme) BottomBarPreviewDark() else BottomBarPreview()
+            }
+
         }
     }
 }
 
 @Composable
 @Preview
-fun ProfileScreen_Preview() {
-    ProfileScreen(true)
+fun ConfigurationScreen_Preview() {
+    MyApplicationTheme(false){
+        ConfigurationScreen(false)
+    }
 }
 
 @Composable
 @Preview
-fun ProfileScreen_PreviewDark() {
-    ProfileScreen(false)
+fun ConfigurationScreen_PreviewDark() {
+    MyApplicationTheme(true){
+        ConfigurationScreen(true)
+    }
 }
