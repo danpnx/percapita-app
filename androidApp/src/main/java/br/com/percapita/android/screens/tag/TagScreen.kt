d802db660c24cd4c700251b40164b64b32111e41
Anderson <anderson.c-morais@hotmail.com>
2023-02-05 02:19:35 -0300

    iniciada a tela de tag, na qual o usuario vai ter acesso as suas tags, podendo edita-las ou cria-las

--- androidApp/src/main/java/br/com/percapita/android/screens/tag/TagScreen.kt
+++ androidApp/src/main/java/br/com/percapita/android/screens/tag/TagScreen.kt
@@ -3,30 +3,41 @@ package br.com.percapita.android.screens.tag
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.percapita.android.MyApplicationTheme
+import br.com.percapita.android.components.BottomBar
+import br.com.percapita.android.components.TagCard
+import br.com.percapita.android.components.TopBar
+import br.com.percapita.android.screens.history.AddTransactionFAB
+import br.com.percapita.android.screens.history.HistoryTopBar
+import br.com.percapita.android.util.Lists.tagList
 
 @Composable
-fun TagScreen() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(
-            modifier = Modifier
-                .padding(16.dp),
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.Center
+fun TagScreen(isSystemDarkTheme: Boolean) {
+    MyApplicationTheme(darkTheme = isSystemDarkTheme) {
+        Scaffold(
+            bottomBar = { BottomBar() },
+            topBar = { TopBar(title = "Tags") }
         ) {
-            items(10) {
-                TagScreen()
+            LazyColumn(
+                modifier = Modifier.padding(it),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                items(tagList) { tag ->
+                    TagCard(tag = tag)
+                }
             }
         }
     }
 }
-
 @Composable
 @Preview
 fun TagScreen_Preview() {
-    TagScreen()
+    TagScreen(isSystemDarkTheme = false)
 }
