9fd3d1369e07a31ab1954fe7df2ba974ecfc11ff
Anderson <anderson.c-morais@hotmail.com>
2023-02-05 18:08:06 -0300

    pequenas mudanças na forma que o card de tags e visualizado na tela

--- androidApp/src/main/java/br/com/percapita/android/components/TagCard.kt
+++ androidApp/src/main/java/br/com/percapita/android/components/TagCard.kt
@@ -3,12 +3,15 @@ package br.com.percapita.android.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Brightness4
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,44 +24,56 @@ import br.com.percapita.model.Tag
 fun TagCard(
     tag: Tag
 ) {
-    MyApplicationTheme {
-        Card(
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 6.dp)
+            .height(50.dp),
+        colors = CardDefaults.cardColors(MaterialTheme.colors.surface),
+        shape = RectangleShape,
+        elevation = CardDefaults.cardElevation(2.dp)
+    )
+        {
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 1.dp)
-                .height(50.dp),
-            colors = CardDefaults.cardColors(MaterialTheme.colors.surface),
-            shape = RectangleShape,
-            elevation = CardDefaults.cardElevation(2.dp)
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .clickable { }
+                .padding(horizontal = 16.dp)
+                .clickable { }
             ) {
-                Row(verticalAlignment = Alignment.CenterVertically)
+            Row(verticalAlignment = Alignment.CenterVertically)
                 {
                     Column() {
                         Text(
                             text = tag.tagName,
                             fontSize = 16.sp,
-                            fontWeight = FontWeight.SemiBold
+                            fontWeight = FontWeight.SemiBold,
+                            color = MaterialTheme.colors.onSurface
                         )
                     }
-                    val iconOptions = Icons.Filled.Edit
                     Spacer(modifier = Modifier.weight(1f))
                     IconButton(onClick = { }) {
-                        Icon(imageVector = iconOptions, contentDescription = "Editar")
+                        Icon(
+                            Icons.Default.Edit,
+                            contentDescription = "Dark Mode",
+                            tint = Color.Gray)
                     }
                 }
             }
         }
-    }
 }
 
 @Composable
 @Preview
 fun TagCard_Preview() {
-    TagCard(tag = Tag("1", "ifood"))
+    MyApplicationTheme(darkTheme = false) {
+        TagCard(tag = Tag("1", "ifood"))
+    }
+}
+
+@Composable
+@Preview
+fun TagCardDark_Preview() {
+    MyApplicationTheme(darkTheme = true) {
+        TagCard(tag = Tag("1", "ifood"))
+    }
 }
