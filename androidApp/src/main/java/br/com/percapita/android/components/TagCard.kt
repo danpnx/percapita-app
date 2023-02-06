2e1f186b12e7e93b2deacc9d4b9a9c3fd3a11b21
Anderson <anderson.c-morais@hotmail.com>
2023-02-06 17:08:27 -0300

    alteracao no modelo da tagcard

--- androidApp/src/main/java/br/com/percapita/android/components/TagCard.kt
+++ androidApp/src/main/java/br/com/percapita/android/components/TagCard.kt
@@ -3,14 +3,14 @@ package br.com.percapita.android.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Brightness4
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.material.IconButton
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
@@ -27,13 +27,12 @@ fun TagCard(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 6.dp)
+            .padding(bottom = 1.dp)
             .height(50.dp),
         colors = CardDefaults.cardColors(MaterialTheme.colors.surface),
         shape = RectangleShape,
         elevation = CardDefaults.cardElevation(2.dp)
-    )
-        {
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -47,26 +46,46 @@ fun TagCard(
                             text = tag.tagName,
                             fontSize = 16.sp,
                             fontWeight = FontWeight.SemiBold,
-                            color = MaterialTheme.colors.onSurface
+                            color = MaterialTheme.colors.onBackground
                         )
                     }
-                    Spacer(modifier = Modifier.weight(1f))
-                    IconButton(onClick = { }) {
-                        Icon(
-                            Icons.Default.Edit,
-                            contentDescription = "Dark Mode",
-                            tint = Color.Gray)
-                    }
+                    DropdownMenuTag()
                 }
             }
         }
 }
 
 @Composable
+fun DropdownMenuTag() {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.TopEnd)) {
+        IconButton(onClick = { expanded = true }) {
+            Icon(
+                Icons.Default.MoreVert,
+                contentDescription = "Localized description",
+                tint = Color.Gray
+            )
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(onClick = { }) {
+                Text("Editar")
+            }
+            DropdownMenuItem(onClick = { }) {
+                Text("Deletar")
+            }
+        }
+    }
+}
+
+@Composable
 @Preview
 fun TagCard_Preview() {
     MyApplicationTheme(darkTheme = false) {
-        TagCard(tag = Tag("1", "ifood"))
+        TagCard(tag = Tag("1", "iFood"))
     }
 }
 
@@ -74,6 +93,6 @@ fun TagCard_Preview() {
 @Preview
 fun TagCardDark_Preview() {
     MyApplicationTheme(darkTheme = true) {
-        TagCard(tag = Tag("1", "ifood"))
+        TagCard(tag = Tag("1", "iFood"))
     }
 }
