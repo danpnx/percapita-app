2caea8d331efff41343d285ba744f15764822a32
Leonardo <leocezar@gmail.com>
2023-02-08 17:42:57 -0300

    Tela HomeScreen

--- androidApp/src/main/java/br/com/percapita/android/screens/home/HomeScreen.kt
+++ androidApp/src/main/java/br/com/percapita/android/screens/home/HomeScreen.kt
@@ -1,18 +1,20 @@
 package br.com.percapita.android.screens.home
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.percapita.android.MyApplicationTheme
 import br.com.percapita.android.components.BottomBar
-
-/**
- * @project PerCapita
- * @author Daniel Augusto on 02/02/2023
- **/
 
 @Composable
 fun HomeScreen(isSystemDarkTheme: Boolean) {
@@ -21,9 +23,50 @@ fun HomeScreen(isSystemDarkTheme: Boolean) {
             bottomBar = { BottomBar() },
             scaffoldState = rememberScaffoldState()
         ) {
-            LazyColumn(modifier = Modifier.padding(it)) {
+            LazyColumn(modifier = Modifier.padding(it).padding(horizontal = 10.dp)) {
+                item {
+                    Card(
+                        shape = CardDefaults.elevatedShape,
+                        elevation = CardDefaults.cardElevation(1.dp),
+                        modifier = Modifier.padding(top = 150.dp).height(100.dp).fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Receitas",
+                            Modifier.padding(start = 10.dp, top = 5.dp)
+                        )
+                    }
+                    Card(
+                        shape = CardDefaults.elevatedShape,
+                        elevation = CardDefaults.cardElevation(1.dp),
+                        modifier = Modifier.padding(top = 20.dp).height(100.dp).fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Despesas",
+                            Modifier.padding(start = 10.dp, top = 5.dp)
+                        )
+                    }
+                    Card(
+                        shape = CardDefaults.elevatedShape,
+                        elevation = CardDefaults.cardElevation(1.dp),
+                        modifier = Modifier.padding(top = 20.dp).height(100.dp).fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Últimos Registros",
+                            Modifier.padding(start = 10.dp, top = 5.dp)
+                        )
+                    }
 
+                }
             }
         }
     }
 }
+
+
+@Composable
+@Preview
+fun HomeScreen_Preview() {
+    HomeScreen(false)
+}
+
+
