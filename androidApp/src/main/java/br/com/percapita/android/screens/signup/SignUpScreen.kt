d19b1d5379bdfb61da844e3a209cba280fe5752f
Anderson <anderson.c-morais@hotmail.com>
2023-02-08 19:10:28 -0300

    alteracoes nas caixas outlinedtext e funcoes aos botoes

--- androidApp/src/main/java/br/com/percapita/android/screens/signup/SignUpScreen.kt
+++ androidApp/src/main/java/br/com/percapita/android/screens/signup/SignUpScreen.kt
@@ -1,5 +1,6 @@
 package br.com.percapita.android.screens.signup
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
@@ -14,7 +15,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -28,7 +31,7 @@ import br.com.percapita.android.MyApplicationTheme
 import br.com.percapita.android.R
 
 @Composable
-fun SignUpScreen() {
+fun SignUpScreen(onLoginNavigation: () -> Unit) {
     MyApplicationTheme(darkTheme = false) {
         Surface(modifier = Modifier.fillMaxSize()) {
             Column(verticalArrangement = Arrangement.Top,
@@ -49,6 +52,7 @@ fun SignUpScreen() {
                 val password = remember { mutableStateOf(TextFieldValue()) }
                 val confirmPassword = remember { mutableStateOf(TextFieldValue()) }
                 val passwordVisible = remember { mutableStateOf(false) }
+                val context = LocalContext.current
                 
                 Text(
                     text = "Pronto para colocar sua renda em ordem?", fontSize = 19.sp,
@@ -59,56 +63,40 @@ fun SignUpScreen() {
                     fontWeight = FontWeight.Light
                 )
                 Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    text = "Nome",
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.fillMaxWidth(0.9f)
-                )
+
                 OutlinedTextField(
                     value = name.value, onValueChange = { name.value = it },
-                    label = { Text(text = "Nome") }, modifier = Modifier.fillMaxWidth(0.9f)
+                    label = { Text(text = "Nome") }, modifier = Modifier.fillMaxWidth(0.9f),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
                 )
                 Spacer(modifier = Modifier.height(15.dp))
 
-                Text(
-                    text = "E-mail",
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.fillMaxWidth(0.9f)
-                )
                 OutlinedTextField(
                     value = email.value, onValueChange = { email.value = it },
-                    label = { Text(text = "E-mail") }, modifier = Modifier.fillMaxWidth(0.9f)
+                    label = { Text(text = "E-mail") }, modifier = Modifier.fillMaxWidth(0.9f),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
                 )
                 Spacer(modifier = Modifier.height(15.dp))
 
-                Text(
-                    text = "Senha",
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.fillMaxWidth(0.9f)
-                )
                 OutlinedTextField(value = password.value, onValueChange = { password.value = it },
                     label = { Text(text = "Senha") }, modifier = Modifier.fillMaxWidth(0.9f),
+                    singleLine = true,
                     visualTransformation = if (passwordVisible.value.not()) PasswordVisualTransformation() else VisualTransformation.None,
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                     trailingIcon = {
-                        val icone =
-                            if (passwordVisible.value.not()) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
-                        val description =
-                            if (passwordVisible.value.not()) "Invisivel" else "Visivel"
+                        val icone = if (passwordVisible.value.not()) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                        val description = if (passwordVisible.value.not()) "Invisivel" else "Visivel"
                         IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
                             Icon(imageVector = icone, contentDescription = description)
                         }
                     })
                 Spacer(modifier = Modifier.height(15.dp))
 
-                Text(
-                    text = "Confirme sua senha",
-                    textAlign = TextAlign.Left,
-                    modifier = Modifier.fillMaxWidth(0.9f)
-                )
                 OutlinedTextField(value = confirmPassword.value,
                     onValueChange = { confirmPassword.value = it },
-                    label = { Text(text = "Senha") },
+                    label = { Text(text = "Confirme sua senha") },
                     modifier = Modifier.fillMaxWidth(0.9f),
                     visualTransformation = if (passwordVisible.value.not()) PasswordVisualTransformation() else VisualTransformation.None,
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
@@ -124,7 +112,9 @@ fun SignUpScreen() {
                 Spacer(modifier = Modifier.height(30.dp))
 
                 Button(
-                    onClick = { /*TODO*/ },
+                    onClick = { onLoginNavigation.invoke()
+                                Toast.makeText(context, "Conta criada com sucesso", Toast.LENGTH_SHORT).show()
+                    },
                     colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF04C457)),
                     modifier = Modifier.fillMaxWidth(0.8f)
                 ) {
@@ -138,5 +128,5 @@ fun SignUpScreen() {
 @Composable
 @Preview
 fun SignUpScreen_Preview() {
-    SignUpScreen()
+    SignUpScreen({})
 }
