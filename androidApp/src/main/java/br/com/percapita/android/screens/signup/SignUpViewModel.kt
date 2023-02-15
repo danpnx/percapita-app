36190011a038064d8b0c0ea4074f91e84e9399d6
Anderson <anderson.c-morais@hotmail.com>
2023-02-15 00:59:48 -0300

    adicionada a classe SignUpViewModel que faz as requisicoes da api para o front

---
+++ androidApp/src/main/java/br/com/percapita/android/screens/signup/SignUpViewModel.kt
@@ -0,0 +1,27 @@
+package br.com.percapita.android.screens.signup
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.percapita.model.User
+import br.com.percapita.repository.SignUpRepository
+import br.com.percapita.utils.DataResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class SignUpViewModel(
+    private val repository: SignUpRepository = SignUpRepository.instance
+): ViewModel() {
+
+    private val _signUp: MutableStateFlow<DataResult<User>> = MutableStateFlow(DataResult.Empty)
+    val signUp: StateFlow<DataResult<User>> = _signUp
+
+    fun signUp(username: String, password: String, name: String) = viewModelScope.launch {
+        val signUp = User(username, password, name)
+
+        repository.signUp(signUp).collectLatest {
+            _signUp.value = it
+        }
+    }
+}
