d3a49a8894ec6d2566040ea93b0c119d00cefa0d
Anderson <anderson.c-morais@hotmail.com>
2023-02-15 00:59:19 -0300

    adicionada a classe LoginiewModel que faz as requisicoes da api para o front

---
+++ androidApp/src/main/java/br/com/percapita/android/screens/login/LoginViewModel.kt
@@ -0,0 +1,26 @@
+package br.com.percapita.android.screens.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.percapita.model.Login
+import br.com.percapita.model.ProfileToken
+import br.com.percapita.repository.LoginRepository
+import br.com.percapita.utils.DataResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class LoginViewModel(private val repository: LoginRepository = LoginRepository.instance) : ViewModel() {
+
+    private val _loginState: MutableStateFlow<DataResult<ProfileToken>> = MutableStateFlow(DataResult.Empty)
+    val loginState: StateFlow<DataResult<ProfileToken>> = _loginState
+
+    fun login(username: String, password: String) = viewModelScope.launch {
+        val login = Login(username, password)
+
+        repository.login(login).collectLatest {
+            _loginState.value = it
+        }
+    }
+}
