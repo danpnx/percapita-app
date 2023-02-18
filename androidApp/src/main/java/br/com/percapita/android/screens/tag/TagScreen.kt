ea354f30e6c728551134f78efcac7f7fd0a54580
Anderson <anderson.c-morais@hotmail.com>
2023-02-18 20:24:06 -0300

    atualizado o codigo de criacao da tag screen, tera outras alteracoes pois ainda nao esta completa

--- androidApp/src/main/java/br/com/percapita/android/screens/tag/TagScreen.kt
+++ androidApp/src/main/java/br/com/percapita/android/screens/tag/TagScreen.kt
@@ -3,7 +3,6 @@ package br.com.percapita.android.screens.tag
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,10 +11,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import br.com.percapita.android.MyApplicationTheme
 import br.com.percapita.android.components.TagCard
 import br.com.percapita.android.components.TopBar
-import br.com.percapita.android.util.Lists.tagList
+import br.com.percapita.utils.DataResult
+import br.com.percapita.model.Tag
+import br.com.percapita.payload.TagList
 
 @Composable
 fun TagScreen(isSystemDarkTheme: Boolean, onBack: () -> Unit, onCreateTag: () -> Unit) {
@@ -23,21 +25,30 @@ fun TagScreen(isSystemDarkTheme: Boolean, onBack: () -> Unit, onCreateTag: () ->
         Scaffold(
             topBar = { TopBar(title = "Tags", onBack = onBack) },
             floatingActionButton = { AddTag(onCreateTag) }
-        ) {
-            Column {
-                Spacer(modifier = Modifier.height(16.dp))
-                LazyColumn(
-                    modifier = Modifier
-                        .padding(it)
-                        .height(445.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    items(tagList) { tag ->
-                        TagCard(tag = tag)
-                    }
-                }
+        ) { _ ->
+            val viewModel = viewModel<TagScreenViewModel>()
+            val tag = viewModel.tag.collectAsState()
+
+            when(tag) {
+                is DataResult.Success<*> -> ContentTagScreen(tag as DataResult.Success<TagList>)
+                else -> Unit
             }
+        }
+    }
+}
+
+@Composable
+fun ContentTagScreen(result: DataResult.Success<TagList>) {
+    val tags = result.data
+    LazyColumn(
+        modifier = Modifier
+            .padding()
+            .height(445.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        items(tags.list.size) {
+            TagCard(tagName = tags.list[it].tagName)
         }
     }
 }
