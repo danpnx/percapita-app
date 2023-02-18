e0e5555332d61beaf81522629667fb0acf4bb120
Anderson <anderson.c-morais@hotmail.com>
2023-02-18 20:15:42 -0300

    adicionada a classe user ao projeto

---
+++ shared/src/commonMain/kotlin/br/com/percapita/model/User.kt
@@ -0,0 +1,16 @@
+package br.com.percapita.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val username: String,
+    val password: String,
+    val name: String
+) {
+}
+
+@Serializable
+data class ProfileToken(
+    val token: String
+)
