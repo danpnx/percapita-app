df92c12f69ea5effca1c1fb4371377239d2772aa
Anderson <anderson.c-morais@hotmail.com>
2023-02-21 02:14:41 -0300

    adicionado os parametros onEditTag e onDeleteTag no componente TagCard e no DropdownMenu, alem da adicao de um alert dialog caso o usuario queira deletar uma tag existente

--- androidApp/src/main/java/br/com/percapita/android/components/TagCard.kt
+++ androidApp/src/main/java/br/com/percapita/android/components/TagCard.kt
@@ -1,5 +1,7 @@
 package br.com.percapita.android.components
 
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -13,16 +15,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import br.com.percapita.android.MyApplicationTheme
-import br.com.percapita.model.Tag
+import br.com.percapita.android.screens.tag.DeleteTagViewModel
 
 @Composable
 fun TagCard(
-    tagName: String
+    id: String,
+    tagName: String,
+    onEditTag: (String) -> Unit,
+    onDeleteTag: (String) -> Unit
 ) {
     Card(
         modifier = Modifier
@@ -49,17 +56,27 @@ fun TagCard(
                         color = MaterialTheme.colors.onBackground
                     )
                 }
-                DropdownMenuTag()
+                DropdownMenuTag(id, onEditTag, onDeleteTag)
             }
         }
     }
 }
 
 @Composable
-fun DropdownMenuTag() {
+fun DropdownMenuTag(
+    id: String,
+    onEditTag: (String) -> Unit,
+    onDeleteTag: (String) -> Unit,
+) {
     var expanded by remember { mutableStateOf(false) }
+    val showDialog = remember { mutableStateOf(false) }
+    val viewModel = viewModel<DeleteTagViewModel>()
+    val tagState by viewModel.deleteTag.collectAsState()
+    val context = LocalContext.current
 
-    Box(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.TopEnd)) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.TopEnd)) {
         IconButton(onClick = { expanded = true }) {
             Icon(
                 Icons.Default.MoreVert,
@@ -71,13 +88,48 @@ fun DropdownMenuTag() {
             expanded = expanded,
             onDismissRequest = { expanded = false }
         ) {
-            DropdownMenuItem(onClick = { }) {
+            DropdownMenuItem(onClick = { onEditTag.invoke(id) }) {
                 Text("Editar")
             }
-            DropdownMenuItem(onClick = { }) {
+            DropdownMenuItem(onClick = {
+                viewModel.deleteTag("", id = id)
+                showDialog.value = true }) {
                 Text("Deletar")
             }
         }
+    }
+
+    if(showDialog.value) {
+        AlertDialog(
+            modifier = Modifier.background(
+                color = MaterialTheme.colors.surface),
+            title = { Text(text = "")},
+            text = { Text(text = "Você deseja deletar essa tag?",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.onBackground
+            )},
+            onDismissRequest = { showDialog.value = false },
+            confirmButton = {
+                TextButton(onClick = {
+                    onDeleteTag(id)
+                    Toast.makeText(context, "Tag deletada com sucesso!", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text(text = "Sim",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = MaterialTheme.colors.onBackground)
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog.value = false }) {
+                    Text(text = "Não",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = MaterialTheme.colors.onBackground)
+                }
+            }
+        )
     }
 }
 
@@ -85,7 +137,7 @@ fun DropdownMenuTag() {
 @Preview
 fun TagCard_Preview() {
     MyApplicationTheme(darkTheme = false) {
-        TagCard("Teste")
+        TagCard("1", "TesteA", {}, {})
     }
 }
 
@@ -93,6 +145,6 @@ fun TagCard_Preview() {
 @Preview
 fun TagCardDark_Preview() {
     MyApplicationTheme(darkTheme = true) {
-        TagCard("TesteB")
+        TagCard("1", "TesteB", {}, {})
     }
 }
