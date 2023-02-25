8e6a1f0b0f7784a8eedc9e53bb818eb16767b1d9
Anderson <anderson.c-morais@hotmail.com>
2023-02-25 13:48:06 -0300

    Adicionada a classe EditProfileViewModel ao projeto

---
+++ androidApp/src/main/java/br/com/percapita/android/screens/profile/EditProfileViewModel.kt
@@ -0,0 +1,42 @@
+package br.com.percapita.android.screens.profile
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.percapita.model.User
+import br.com.percapita.repository.ProfileRepository
+import br.com.percapita.utils.DataResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class EditProfileViewModel(
+    private val repository: ProfileRepository = ProfileRepository.instance
+): ViewModel() {
+
+    init {
+        getProfile()
+    }
+
+    private val _profile = MutableStateFlow<DataResult<User>>(DataResult.Empty)
+    val profile: StateFlow<DataResult<User>> = _profile
+
+    fun editName(user: User) = viewModelScope.launch {
+        repository.editName(user).collectLatest {
+            _profile.value = it
+        }
+    }
+
+    fun editPassword(user: User) = viewModelScope.launch {
+        repository.editPassword(user).collectLatest {
+            _profile.value = it
+        }
+    }
+
+    fun getProfile() = viewModelScope.launch {
+        repository.getProfile().collectLatest {
+            _profile.value = it
+        }
+    }
+
+}
